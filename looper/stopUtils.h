6893234c908d40b5b98e502e1ae3b355c1332db3
dalfonso <dalfonso>
2012-12-03 20:23:06 +0000

    *** empty log message ***

--- looper/stopUtils.h
+++ looper/stopUtils.h
@@ -85,8 +85,32 @@ pair<float,float> Type1PFMETSmear(JetSmearer* jetSmearer, bool isData,
 
 float getMT( float leppt , float lepphi , float met , float metphi );
 
-float trackIso( int thisPf , float coneR , float dz_thresh , bool dovtxcut , float pt_thresh );
-float trackIso( int thisPf , float coneR = 0.3 , float dz_thresh = 0.05 , bool dovtxcut = false , float pt_thresh = 0.0); 
+struct myTrackIso {
+
+  //defaultValue 
+  float iso_dr03_dz000_pt00;
+
+  // dz variation
+  float iso_dr03_dz005_pt00; // default value
+  float iso_dr03_dz020_pt00;
+
+  //pt Variation
+  float iso_dr03_dz005_pt01;
+  float iso_dr03_dz005_pt02;
+  float iso_dr03_dz005_pt03;
+  float iso_dr03_dz005_pt04;
+  float iso_dr03_dz005_pt05;
+  float iso_dr03_dz005_pt06;
+  float iso_dr03_dz005_pt07;
+  float iso_dr03_dz005_pt08;
+  float iso_dr03_dz005_pt09;
+  float iso_dr03_dz005_pt10;
+
+};
+
+
+struct myTrackIso trackIso( int thisPf , float coneR , float dz_thresh , bool dovtxcut , float pt_thresh );
+struct myTrackIso trackIso( int thisPf , float coneR = 0.3 , float dz_thresh = 0.05 , bool dovtxcut=false, float pt_thresh = 0.0); 
 
 //--------------------------------------------------------------------   
                               
