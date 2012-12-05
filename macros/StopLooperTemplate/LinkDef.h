2acc51e1aaf1a125c33f43ba8da881e98f129249
dalfonso <dalfonso>
2012-12-05 13:18:13 +0000

    first commit

---
+++ macros/StopLooperTemplate/LinkDef.h
@@ -0,0 +1,10 @@
+#include "StopTreeLooper.h"
+
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link C++ all functions;
+
+#pragma link C++ class StopTreeLooper;
+
+#endif
