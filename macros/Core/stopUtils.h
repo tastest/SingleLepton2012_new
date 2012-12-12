bc7dbec46172e292d35c0d9cd355fb7280118a16
benhoob <benhoob>
2012-12-12 10:21:54 +0000

    transfer several functions from the looper code to stopUtils.cc

--- macros/Core/stopUtils.h
+++ macros/Core/stopUtils.h
@@ -25,4 +25,20 @@ using namespace std;
 list<Candidate> MT2Calculator(StopTree* tree, bool isData);
 MT2struct Best_MT2Calculator(StopTree* tree, bool isData);
 
+float getdltrigweight(int id1, int id2);
+float getsltrigweight(int id1, float pt, float eta);
+
+bool passEvtSelection(const StopTree *sTree, TString name);
+bool passOneLeptonSelection(const StopTree *sTree, bool isData);
+bool passTwoLeptonSelection(const StopTree *sTree, bool isData);
+bool passIsoTrkVeto(const StopTree *sTree);
+bool passSingleLeptonSelection(const StopTree *sTree, bool isData);
+bool passDileptonSelection(const StopTree *sTree, bool isData);
+bool passLepPlusIsoTrkSelection(const StopTree *sTree, bool isData);
+pair<float,float> getPhiCorrMET( float met, float metphi, int nvtx, bool ismc);
+
+float vtxweight_n( const int nvertices, TH1F *hist, bool isData );
+float getdphi( float phi1 , float phi2 );
+float dRbetweenVectors(StopTree::LorentzVector vec1,StopTree::LorentzVector vec2 );
+
 #endif
