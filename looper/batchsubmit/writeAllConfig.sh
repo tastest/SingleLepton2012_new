6eda733c72e27600fe859f5eef11f3f905d5df2b
vimartin <vimartin>
2012-12-20 14:48:24 +0000

    updating MG sample

--- looper/batchsubmit/writeAllConfig.sh
+++ looper/batchsubmit/writeAllConfig.sh
@@ -53,7 +53,7 @@ TAG="V00-02-06_2012"
 #
 # TTBAR
 #
-./writeConfig.sh /hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-13_slim/ ${TAG}_TTJetsMG
+./writeConfig.sh /hadoop/cms/store/group/snt/papers2012/Summer12_53X_MC/TTJets_MassiveBinDECAY_TuneZ2star_8TeV-madgraph-tauola_Summer12_DR53X-PU_S10_START53_V7A-v1/V05-03-18_slim/ ${TAG}_TTJetsMG
 
 #
 # ALTERNATIVE TTBAR
