da1953c077dc1d5086ade01b7de04b21507b2626
benhoob <benhoob>
2013-03-07 16:15:18 +0000

    update to single lepton trigger efficiencies measured with full 2012 data

--- macros/Core/stopUtils.cc
+++ macros/Core/stopUtils.cc
@@ -130,112 +130,128 @@ float getdltrigweight(int id1, int id2){
 
 }
 
-//-------------------------------------------
-// efficiency for single lepton trigger
-//-------------------------------------------
-
-float getsltrigweight(int id1, float pt, float eta){
-
+float getsltrigweight(int id1, float pt, float eta) 
+{
+  //--------------------------------------------------------------------------------------
+  // This function returns the trigger efficiencies for HLT_IsoMu24 and HLT_Ele27_WP80
+  // Efficiencies are calculated with the full 2012 data sample using Z tag-and-probe
+  // 3 arguments:
+  // id1 : lepton PDG ID, abs(id1)=11 (electron),  abs(id1)=13 (muon)  
+  // pt  : lepton pt
+  // eta : lepton eta
+  //--------------------------------------------------------------------------------------
   //electron efficiencies
   if ( abs(id1)==11 ) {
+    
     if ( fabs(eta)<1.5) {
-      if ( pt>=20 && pt<22 ) return 0.00;
-      if ( pt>=22 && pt<24 ) return 0.00;
-      if ( pt>=24 && pt<26 ) return 0.00;
-      if ( pt>=26 && pt<28 ) return 0.08;
-      if ( pt>=28 && pt<30 ) return 0.61;
-      if ( pt>=30 && pt<32 ) return 0.86;
-      if ( pt>=32 && pt<34 ) return 0.88;
-      if ( pt>=34 && pt<36 ) return 0.90;
-      if ( pt>=36 && pt<38 ) return 0.91;
-      if ( pt>=38 && pt<40 ) return 0.92;
-      if ( pt>=40 && pt<50 ) return 0.94;
-      if ( pt>=50 && pt<60 ) return 0.95;
-      if ( pt>=60 && pt<80 ) return 0.96;
-      if ( pt>=80 && pt<100 ) return 0.96;
-      if ( pt>=100 && pt<150 ) return 0.96;
+      if ( pt>=20  && pt<22  ) return 0.00;
+      if ( pt>=22  && pt<24  ) return 0.00;
+      if ( pt>=24  && pt<26  ) return 0.00;
+      if ( pt>=26  && pt<28  ) return 0.07;
+      if ( pt>=28  && pt<30  ) return 0.57;
+      if ( pt>=30  && pt<32  ) return 0.85;
+      if ( pt>=32  && pt<34  ) return 0.88;
+      if ( pt>=34  && pt<36  ) return 0.89;
+      if ( pt>=36  && pt<38  ) return 0.91;
+      if ( pt>=38  && pt<40  ) return 0.92;
+      if ( pt>=40  && pt<50  ) return 0.94;
+      if ( pt>=50  && pt<60  ) return 0.95;
+      if ( pt>=60  && pt<80  ) return 0.96;
+      if ( pt>=80  && pt<100 ) return 0.96;
+      if ( pt>=100 && pt<150 ) return 0.97;
       if ( pt>=150 && pt<200 ) return 0.97;
-      if ( pt>=200 ) return 0.97;
-    } else if ( fabs(eta)>=1.5 && fabs(eta)<2.1) {
-      if ( pt>=20 && pt<22 ) return 0.00;
-      if ( pt>=22 && pt<24 ) return 0.00;
-      if ( pt>=24 && pt<26 ) return 0.02;
-      if ( pt>=26 && pt<28 ) return 0.18;
-      if ( pt>=28 && pt<30 ) return 0.50;
-      if ( pt>=30 && pt<32 ) return 0.63;
-      if ( pt>=32 && pt<34 ) return 0.68;
-      if ( pt>=34 && pt<36 ) return 0.70;
-      if ( pt>=36 && pt<38 ) return 0.72;
-      if ( pt>=38 && pt<40 ) return 0.74;
-      if ( pt>=40 && pt<50 ) return 0.76;
-      if ( pt>=50 && pt<60 ) return 0.77;
-      if ( pt>=60 && pt<80 ) return 0.78;
-      if ( pt>=80 && pt<100 ) return 0.80;
-      if ( pt>=100 && pt<150 ) return 0.79;
-      if ( pt>=150 && pt<200 ) return 0.76;
-      if ( pt>=200 ) return 0.81;
+      if ( pt>=200           ) return 0.97;
+    } 
+    else if ( fabs(eta)>=1.5 && fabs(eta)<2.1) {
+      if ( pt>=20  && pt<22  ) return 0.00; 
+      if ( pt>=22  && pt<24  ) return 0.00; 
+      if ( pt>=24  && pt<26  ) return 0.03; 
+      if ( pt>=26  && pt<28  ) return 0.22; 
+      if ( pt>=28  && pt<30  ) return 0.52; 
+      if ( pt>=30  && pt<32  ) return 0.65; 
+      if ( pt>=32  && pt<34  ) return 0.70; 
+      if ( pt>=34  && pt<36  ) return 0.72; 
+      if ( pt>=36  && pt<38  ) return 0.74; 
+      if ( pt>=38  && pt<40  ) return 0.75; 
+      if ( pt>=40  && pt<50  ) return 0.77; 
+      if ( pt>=50  && pt<60  ) return 0.79; 
+      if ( pt>=60  && pt<80  ) return 0.79; 
+      if ( pt>=80  && pt<100 ) return 0.80; 
+      if ( pt>=100 && pt<150 ) return 0.82; 
+      if ( pt>=150 && pt<200 ) return 0.83; 
+      if ( pt>=200           ) return 0.85; 
     }
-  } else if ( abs(id1)==13 ) {//muon efficiencies
-
+    else{
+      std::cout << "WARNING: electron eta " << eta << " is out of range. Return trigger efficiency = 1" << std::endl;
+    }
+  } 
+  //muon efficiencies
+  else if ( abs(id1)==13 ) {
     if ( fabs(eta)<0.8 ) {
-      if (pt>=20 && pt<22)  return  0.00;	 
-      if (pt>=22 && pt<24)  return  0.03; 	 
-      if (pt>=24 && pt<26)  return  0.87; 
-      if (pt>=26 && pt<28)  return  0.90; 
-      if (pt>=28 && pt<30)  return  0.91; 
-      if (pt>=30 && pt<32)  return  0.91; 
-      if (pt>=32 && pt<34)  return  0.92; 
-      if (pt>=34 && pt<36)  return  0.93; 
-      if (pt>=36 && pt<38)  return  0.93; 
-      if (pt>=38 && pt<40)  return  0.93; 
-      if (pt>=40 && pt<50)  return  0.94; 
-      if (pt>=50 && pt<60)  return  0.95; 
-      if (pt>=60 && pt<80)  return  0.95; 
-      if (pt>=80 && pt<100) return 0.94; 
-      if (pt>=100 && pt<150) return 0.94; 
-      if (pt>=150 && pt<200) return 0.93; 
-      if (pt>=200) return 0.92; 
-    } else if ( fabs(eta)>=0.8 && fabs(eta)<1.5 ) {
-      if (pt>=20 && pt<22)  return  0.00;
-      if (pt>=22 && pt<24)  return  0.05;
-      if (pt>=24 && pt<26)  return  0.78;
-      if (pt>=26 && pt<28)  return  0.81;
-      if (pt>=28 && pt<30)  return  0.81;
-      if (pt>=30 && pt<32)  return  0.81;
-      if (pt>=32 && pt<34)  return  0.82;
-      if (pt>=34 && pt<36)  return  0.82;
-      if (pt>=36 && pt<38)  return  0.83;
-      if (pt>=38 && pt<40)  return  0.83;
-      if (pt>=40 && pt<50)  return  0.84;
-      if (pt>=50 && pt<60)  return  0.84;
-      if (pt>=60 && pt<80)  return  0.84;
-      if (pt>=80 && pt<100) return 0.84; 
-      if (pt>=100 && pt<150) return 0.84;
-      if (pt>=150 && pt<200) return 0.84;
-      if (pt>=200) return 0.82;
-    } else if ( fabs(eta)>=1.5 && fabs(eta)<2.1 ) {
-      if (pt>=20 && pt<22)  return  0.00;
-      if (pt>=22 && pt<24)  return  0.11;
-      if (pt>=24 && pt<26)  return  0.76;
-      if (pt>=26 && pt<28)  return  0.78;
-      if (pt>=28 && pt<30)  return  0.79;
-      if (pt>=30 && pt<32)  return  0.80;
-      if (pt>=32 && pt<34)  return  0.80;
-      if (pt>=34 && pt<36)  return  0.81;
-      if (pt>=36 && pt<38)  return  0.81;
-      if (pt>=38 && pt<40)  return  0.82;
-      if (pt>=40 && pt<50)  return  0.82;
-      if (pt>=50 && pt<60)  return  0.83;
-      if (pt>=60 && pt<80)  return  0.83;
-      if (pt>=80 && pt<100) return 0.83;
-      if (pt>=100 && pt<150) return 0.83;
-      if (pt>=150 && pt<200) return 0.82;
-      if (pt>=200) return 0.82;
+      if (pt>=20  && pt<22  ) return 0.00;
+      if (pt>=22  && pt<24  ) return 0.02;
+      if (pt>=24  && pt<26  ) return 0.87;
+      if (pt>=26  && pt<28  ) return 0.90;
+      if (pt>=28  && pt<30  ) return 0.91;
+      if (pt>=30  && pt<32  ) return 0.91;
+      if (pt>=32  && pt<34  ) return 0.92;
+      if (pt>=34  && pt<36  ) return 0.93;
+      if (pt>=36  && pt<38  ) return 0.93;
+      if (pt>=38  && pt<40  ) return 0.93;
+      if (pt>=40  && pt<50  ) return 0.94;
+      if (pt>=50  && pt<60  ) return 0.95;
+      if (pt>=60  && pt<80  ) return 0.95;
+      if (pt>=80  && pt<100 ) return 0.94;
+      if (pt>=100 && pt<150 ) return 0.94;
+      if (pt>=150 && pt<200 ) return 0.93;
+      if (pt>=200           ) return 0.92;
+    } 
+    else if ( fabs(eta)>=0.8 && fabs(eta)<1.5 ) {
+      if (pt>=20  && pt<22  ) return 0.00;
+      if (pt>=22  && pt<24  ) return 0.05;
+      if (pt>=24  && pt<26  ) return 0.78;
+      if (pt>=26  && pt<28  ) return 0.80;
+      if (pt>=28  && pt<30  ) return 0.81;
+      if (pt>=30  && pt<32  ) return 0.82;
+      if (pt>=32  && pt<34  ) return 0.82;
+      if (pt>=34  && pt<36  ) return 0.82;
+      if (pt>=36  && pt<38  ) return 0.83;
+      if (pt>=38  && pt<40  ) return 0.83;
+      if (pt>=40  && pt<50  ) return 0.84;
+      if (pt>=50  && pt<60  ) return 0.84;
+      if (pt>=60  && pt<80  ) return 0.84;
+      if (pt>=80  && pt<100 ) return 0.84;
+      if (pt>=100 && pt<150 ) return 0.84;
+      if (pt>=150 && pt<200 ) return 0.83;
+      if (pt>=200           ) return 0.83;
+    } 
+    else if ( fabs(eta)>=1.5 && fabs(eta)<2.1 ) {
+      if (pt>=20 && pt<22   ) return 0.00;
+      if (pt>=22 && pt<24   ) return 0.10;
+      if (pt>=24 && pt<26   ) return 0.76;
+      if (pt>=26 && pt<28   ) return 0.78;
+      if (pt>=28 && pt<30   ) return 0.79;
+      if (pt>=30 && pt<32   ) return 0.80;
+      if (pt>=32 && pt<34   ) return 0.81;
+      if (pt>=34 && pt<36   ) return 0.81;
+      if (pt>=36 && pt<38   ) return 0.82;
+      if (pt>=38 && pt<40   ) return 0.82;
+      if (pt>=40 && pt<50   ) return 0.83;
+      if (pt>=50 && pt<60   ) return 0.83;
+      if (pt>=60 && pt<80   ) return 0.84;
+      if (pt>=80 && pt<100  ) return 0.84;
+      if (pt>=100 && pt<150 ) return 0.84;
+      if (pt>=150 && pt<200 ) return 0.82;
+      if (pt>=200           ) return 0.83;
     }
-  }//end check for muons
-
+    else{
+      std::cout << "WARNING: muon eta " << eta << " is out of range. Return trigger efficiency = 1" << std::endl;
+    }
+  }
+  else{
+    std::cout << "WARNING: unrecognized lepton id " << id1 << ". Return trigger efficiency = 1" << std::endl;
+  }
   return 1.;
-
 }
 
 //-----------------------------------------------------------------------
