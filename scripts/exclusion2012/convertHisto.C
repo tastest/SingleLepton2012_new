2b0b3e9e0b3e7420e9a1adb16750e592ee408c96
benhoob <benhoob>
2013-04-03 11:11:49 +0000

    minor update

--- scripts/exclusion2012/convertHisto.C
+++ scripts/exclusion2012/convertHisto.C
@@ -10,9 +10,15 @@
   char* filename_out = "/tas/cms2/stop/cms2V05-03-25_stoplooperV00-02-18/T2bw/minibabyV00-03-03/myMassDB_T2bw_25GeVbins.root";
 
   TFile* fin = TFile::Open(filename_in);
-  TH2F*  hin = (TH2F*) fin->Get("masses");
-  
-  TH2F*  hout = new TH2F("masses","masses",41,-12.5,1012.5,41,-12.5,1012.5); 
+
+  TH2F*  hin    = (TH2F*) fin->Get("masses");
+  TH2F*  hout   = new TH2F("masses","masses",41,-12.5,1012.5,41,-12.5,1012.5); 
+
+  TH2F*  hin25  = (TH2F*) fin->Get("masses25");
+  TH2F*  hout25 = new TH2F("masses25","masses25",41,-12.5,1012.5,41,-12.5,1012.5); 
+
+  TH2F*  hin75  = (TH2F*) fin->Get("masses75");
+  TH2F*  hout75 = new TH2F("masses75","masses75",41,-12.5,1012.5,41,-12.5,1012.5); 
 
   for( int ibin = 1 ; ibin <= 41 ; ibin++ ){
     for( int jbin = 1 ; jbin <= 41 ; jbin++ ){
@@ -27,6 +33,12 @@
       
       hout->SetBinContent(ibin,jbin,nevents);
 
+      int nevents25 = hin25->GetBinContent(bin);
+      int nevents75 = hin75->GetBinContent(bin);
+
+      hout25->SetBinContent(ibin,jbin,nevents25);
+      hout75->SetBinContent(ibin,jbin,nevents75);
+
     }
   }
 
@@ -35,6 +47,8 @@
   TFile *fout = TFile::Open(filename_out,"RECREATE");
   fout->cd();
   hout->Write();
+  hout25->Write();
+  hout75->Write();
   fout->Close();
 
 
