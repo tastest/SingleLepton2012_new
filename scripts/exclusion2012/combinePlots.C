9d4417c93670b2d49f581343c79602b9076d9a8e
benhoob <benhoob>
2013-04-07 18:56:49 +0000

    style updates

--- scripts/exclusion2012/combinePlots.C
+++ scripts/exclusion2012/combinePlots.C
@@ -34,9 +34,33 @@ void removeSlice( TH2F* h ){
     h->SetBinContent(ibin,1,1000);
 }
 
-TH2F* largeDM( TH2F* hin ){
+void smoothHistogram( TH2F* h ){
+
+  for( int ibin = 1 ; ibin <= h->GetXaxis()->GetNbins() ; ibin++ ){
+    for( int jbin = 1 ; jbin <= h->GetYaxis()->GetNbins() ; jbin++ ){
+
+      float val     = h->GetBinContent(ibin,jbin);
+      float valup   = h->GetBinContent(ibin+1,jbin);
+      float valdown = h->GetBinContent(ibin-1,jbin);
+
+      float valyup   = h->GetBinContent(ibin,jbin+1);
+      float valydown = h->GetBinContent(ibin,jbin-1);
+
+      if( val < 1e-10 && valup > 1e-10 && valdown > 1e-10 ){
+	h->SetBinContent(ibin,jbin,0.5*(valup+valdown));
+      }
+
+      if( val < 1e-10 && valyup > 1e-10 && valydown > 1e-10 ){
+	h->SetBinContent(ibin,jbin,0.5*(valyup+valydown));
+      }
+    }
+  }
+}
+
+TH2F* largeDM( TH2F* hin , char* sample ){
   TH2F* hout = (TH2F*) hin->Clone("hout");
 
+  if( TString(sample).Contains("T2bw") ) return hout;
 
   for( int ibin = 1 ; ibin <= hin->GetXaxis()->GetNbins() ; ibin++ ){
     for( int jbin = 1 ; jbin <= hin->GetYaxis()->GetNbins() ; jbin++ ){
@@ -201,7 +225,7 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
 
   if( TString(sample).Contains("T2tt") ){
     label       = "pp #rightarrow #tilde{t} #tilde{t}, #tilde{t} #rightarrow t #chi_{1}^{0}";
-    xaxismin    =   0.0;
+    xaxismin    = 100.0;
     yaxismax    = 250.0;
 
     if( doBDT ) nSR = 6;
@@ -420,6 +444,12 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   if( TString(sample).Contains("T2bw") && x==75 ) t->DrawLatex(0.15,0.03,"m_{#chi_{1}^{#pm}} = 0.75 m_{ #tilde{t}} + 0.25 m_{#chi_{1}^{0}}");
 
 
+  if( TString(sample).Contains("T2bw") && x == 75 ){
+    smoothHistogram( hxsec_best );
+    smoothHistogram( hxsec_best_exp );
+    smoothHistogram( hxsec_best_expp1 );
+    smoothHistogram( hxsec_best_expm1 );
+  }
 
 
   //-------------------------------
@@ -504,16 +534,17 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   //   }
   // }
 
-  TH2F* hxsec_best_exp_largeDM = largeDM( hxsec_best_exp);
-  TH2F* hxsec_best_exp_smallDM = smallDM( hxsec_best_exp);
 
-  TH2F* hxsec_best_expp1_largeDM = largeDM( hxsec_best_expp1);
-  TH2F* hxsec_best_expp1_smallDM = smallDM( hxsec_best_expp1);
+  TH2F* hxsec_best_exp_largeDM = largeDM( hxsec_best_exp , sample );
+  TH2F* hxsec_best_exp_smallDM = smallDM( hxsec_best_exp );
 
-  TH2F* hxsec_best_expm1_largeDM = largeDM( hxsec_best_expm1);
+  TH2F* hxsec_best_expp1_largeDM = largeDM( hxsec_best_expp1 , sample );
+  TH2F* hxsec_best_expp1_smallDM = smallDM( hxsec_best_expp1 );
+
+  TH2F* hxsec_best_expm1_largeDM = largeDM( hxsec_best_expm1 , sample );
   TH2F* hxsec_best_expm1_smallDM = smallDM( hxsec_best_expm1);
 
-  TH2F* hxsec_best_largeDM = largeDM( hxsec_best );
+  TH2F* hxsec_best_largeDM = largeDM( hxsec_best , sample);
   TH2F* hxsec_best_smallDM = smallDM( hxsec_best );
 
   // TGraph* gr        = getRefXsecGraph(hxsec_best       , "T2tt", 1.0);
@@ -707,7 +738,7 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   hxsec_best_shifted->Draw("samecolz");
   hdummy->Draw("axissame");
   //hexcl_exp->Draw("samebox");
-  hexcl->Draw("samebox");
+  //hexcl->Draw("samebox");
 
   TGraph* HCP;
   if     ( TString(sample).Contains("T2tt") )  HCP   = T2tt_expected();
@@ -872,11 +903,11 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   t->DrawLatex(0.18,0.84,label);
   t->SetTextSize(0.04);
   t->DrawLatex(0.50,0.85  ,"NLO-NLL exclusions");
-  //t->DrawLatex(0.55,0.80,"Observed #pm1#sigma^{theory}");
-  //t->DrawLatex(0.55,0.75,"Expected #pm1#sigma");
-  if( doBDT ) t->DrawLatex(0.55,0.80,"MVA expected");
+  t->DrawLatex(0.55,0.80,"Observed");
+  t->DrawLatex(0.55,0.75,"Expected #pm1#sigma");
+  //if( doBDT ) t->DrawLatex(0.55,0.80,"Expected (#pm1#sigma)");
   //else        t->DrawLatex(0.55,0.80,"C&C expected");
-  else        t->DrawLatex(0.55,0.80,"cut-and-count");
+  //else        t->DrawLatex(0.55,0.80,"cut-and-count");
 
 
   t->SetTextSize(0.045);
@@ -894,28 +925,36 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   // float yspace1 =     5;
   // float yspace2 =    17;
 
-  float xoffset = 370.0;
-  float yoffset = 338.0;
+  float xoffset = xaxismin+370.0;
+  float yoffset = 330.0;
   float length  =  40.0;
   float yspace1 =     5;
-  float yspace2 =    17;
+  float yspace2 =    25;
 
-  /*
-  if( TString(sample).Contains("T2bw") && x==75 ) xoffset -= 30.0;
+
+  if( TString(sample).Contains("T2bw") && x==25 ){
+    xoffset -= 130.0;
+    length = 20;
+  }
+  if( TString(sample).Contains("T2bw") && x==50 ){
+    xoffset -= 40.0;
+    length = 30;
+  }
+  //if( TString(sample).Contains("T2bw") && x==75 ) xoffset -= 30.0;
   
   // median expected
   //TLine *line22 = new TLine(xaxismin+xoffset+25, 310-yoffset, xaxismin+xoffset+65, 310-yoffset);
   TLine *line22 = new TLine(xoffset,yoffset,xoffset+length,yoffset);
   line22->SetLineWidth(4);
-  line22->SetLineColor(4);
-  line22->SetLineStyle(7);
+  line22->SetLineColor(2);
+  line22->SetLineStyle(1);
   line22->Draw();
  
   // expected +/-1sigma
   //TLine *line23 = new TLine(xaxismin+xoffset+25, 317-yoffset, xaxismin+xoffset+65, 317-yoffset);
   TLine *line23 = new TLine(xoffset,yoffset+yspace1,xoffset+length,yoffset+yspace1);
   line23->SetLineWidth(3);
-  line23->SetLineColor(4);
+  line23->SetLineColor(2);
   line23->SetLineStyle(3);
   line23->Draw();
 
@@ -923,17 +962,18 @@ void combinePlots(char* sample = "T2tt" , int x = 1, bool doBDT = false, bool pr
   //TLine *line24 = new TLine(xaxismin+xoffset+25, 303-yoffset, xaxismin+xoffset+65, 303-yoffset);
   TLine *line24 = new TLine(xoffset,yoffset-yspace1,xoffset+length,yoffset-yspace1);
   line24->SetLineWidth(3);
-  line24->SetLineColor(4);
+  line24->SetLineColor(2);
   line24->SetLineStyle(3);
   line24->Draw();
-  */
+
   // median observed
   //TLine *line25 = new TLine(xaxismin+xoffset+25, 335-yoffset, xaxismin+xoffset+65, 335-yoffset);
   TLine *line25 = new TLine(xoffset,yoffset+yspace2,xoffset+length,yoffset+yspace2);
   line25->SetLineWidth(6);
   line25->SetLineColor(1);
   line25->SetLineStyle(1);
-  //line25->Draw();
+  line25->Draw();
+
   /*
   //TLine *line26 = new TLine(xaxismin+xoffset+25, 342-yoffset, xaxismin+xoffset+65, 342-yoffset);
   TLine *line26 = new TLine(xoffset,yoffset+yspace1+yspace2,xoffset+length,yoffset+yspace1+yspace2);
@@ -1005,4 +1045,9 @@ void doAll(){
   combinePlots("T2bw",25,false,true);
   combinePlots("T2bw",50,false,true);
   combinePlots("T2bw",75,false,true);
+
+  combinePlots("T2tt", 1,true,true);
+  combinePlots("T2bw",25,true,true);
+  combinePlots("T2bw",50,true,true);
+  combinePlots("T2bw",75,true,true);
 }
