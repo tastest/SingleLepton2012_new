befc788a8c3e2d9d7536639db1ba57e712896a7e
vimartin <vimartin>
2013-07-17 22:48:50 +0000

    updating writeconfig

--- looper/batchsubmit/writeConfig.sh
+++ looper/batchsubmit/writeConfig.sh
@@ -5,7 +5,8 @@ do echo "No Proxy found issuing \"voms-proxy-init -voms cms\""
    voms-proxy-init -hours 168 -voms cms
 done
 
-UNIVERSE="grid"
+#UNIVERSE="grid"
+UNIVERSE="vanilla"
 EXE="wrapper.sh"
 INPUT="wrapper.sh, job_input/input.tar.gz"
 SITE="UCSD"
@@ -56,22 +57,22 @@ fi
 #
 # write configuration
 #
-
+   
+#Grid_Resource=gt2 osg-gw-6.t2.ucsd.edu:2119/jobmanager-condor
 echo "
-Grid_Resource=gt2 osg-gw-6.t2.ucsd.edu:2119/jobmanager-condor
 universe=${UNIVERSE}
 when_to_transfer_output = ON_EXIT
 #the actual executable to run is not transfered by its name.
 #In fact, some sites may do weird things like renaming it and such.
 transfer_input_files=${INPUT}
-+DESIRED_Sites=${SITE}
++DESIRED_Sites=\"${SITE}\"
 +Owner = undefined
 log=${LOG}
 output=${OUT}
 error =${ERR}
 notification=Never
 x509userproxy=${PROXY}
-" > condor_${COPYDIRBASE}.cmd
+" > condor_${COPYDIRBASE##*/}.cmd
 
     #
     # now set the rest of the arguments 
@@ -84,8 +85,7 @@ executable=${EXE}
 transfer_executable=True
 arguments=`echo ${FILE##*/} | sed 's/\.root//g'` ${FILE} ${COPYDIR}
 queue
-" >> condor_${COPYDIRBASE}.cmd
+" >> condor_${COPYDIRBASE##*/}.cmd
     done
 
-echo "[writeConfig] wrote condor_${COPYDIRBASE}.cmd" 
-
+echo "[writeConfig] wrote condor_${COPYDIRBASE##*/}.cmd" 
